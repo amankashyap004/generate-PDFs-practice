646ba95475f82c1b3e1928c3e2ffa95e3bd05548
amankashyap004 <amankashyap004@gmail.com>
2023-07-12 18:26:32 +0530

    create using js pdf component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import UsingPDFMake from "./components/UsingPDFMake";
+import UsingJsPDF from "./components/UsingJsPDF";
 import "./App.css";
 
 function App() {
    return (
       <main>
          <UsingPDFMake />
+         <UsingJsPDF />
       </main>
    );
 }
