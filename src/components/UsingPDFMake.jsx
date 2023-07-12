4fbe68218854aaedec79a42db6a0c21678f0fa8d
amankashyap004 <amankashyap004@gmail.com>
2023-07-12 18:22:50 +0530

    pdf make

--- src/components/UsingPDFMake.jsx
+++ src/components/UsingPDFMake.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+
+// Register the fonts required by pdfmake
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const UsingPDFMake = () => {
-   return <div>UsingPDFMake</div>;
+   const generatePDF = () => {
+      // Create a document definition
+      const documentDefinition = {
+         content: [
+            {
+               text: "Hello, World!",
+               fontSize: 16,
+               bold: true,
+               alignment: "center",
+               margin: [0, 0, 0, 20],
+            },
+            { text: "This is a sample PDF generated with pdfmake in React.", fontSize: 12 },
+         ],
+      };
+
+      // Generate the PDF
+      const pdfDocGenerator = pdfMake.createPdf(documentDefinition);
+      pdfDocGenerator.download("sample.pdf");
+   };
+
+   return (
+      <section>
+         <div>
+            <h2>PDF Generate using pdfmake</h2>
+         </div>
+         <div>
+            <button onClick={generatePDF}>Generate PDF</button>
+         </div>
+      </section>
+   );
 };
 
 export default UsingPDFMake;
